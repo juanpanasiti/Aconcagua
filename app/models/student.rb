66393534a99210b872e27c64579e7ff79d5ea439
Juan M. Panasiti <juanpanasiti@gmail.com>
2016-07-10 00:36:24 -0300

    Agregado archivos de fuente Roboto, creado controladores customizables de devise para Student y customizadas las vistas de Login, registro y edit de registro

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,15 @@ class Student < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  ########## METHODS
+  def get_name
+    name = ''
+    #if self.profile.present? && self.profile.name.present?
+    #  name = self.name
+    #else
+      name = self.email.split('@')[0]
+    #end
+    return name
+  end
 end
